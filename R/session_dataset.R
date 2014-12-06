2cb66467d06b5cfab19d1507cd10d6c96dd45e50
ironholds <ironholds@gmail.com>
2014-12-06 18:46:25 -0500

    Fix for issue #8; de-numericises the timestamp in session_dataset

--- R/session_dataset.R
+++ R/session_dataset.R
@@ -3,7 +3,7 @@
 #' @description an example dataset of events, drawn from the Wikimedia web properties, for experimenting with
 #' when performing session reconstruction and analysis
 #'
-#' @format a data.frame of 61,667 rows consisting of:
+#' @format a data.frame of 63,524 rows consisting of:
 #' \describe{
 #'   \item{UUID}{Hashed and salted unique identifiers representing 10,000 unique clients.}
 #'   \item{timestamp}{timestamps, pre-converted using \code{\link{to_seconds}} to be immediately usable by \code{\link{sessioniser}}}
